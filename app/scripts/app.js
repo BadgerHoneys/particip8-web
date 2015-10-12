4083e77cb5d2444752da32b25f0ac2db6c06a862
Josh Woodward <jdw6359@rit.edu>
2015-10-12 18:48:25 +0000

    Initial Project Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name particip8WebApp
+ * @description
+ * # particip8WebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('particip8WebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name particip8WebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the particip8WebApp
+ */
+angular.module('particip8WebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
