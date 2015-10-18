75738e0a00970c5062653bc42b05e5d9338b6738
Josh Woodward <jdw6359@rit.edu>
2015-10-18 18:28:56 -0400

    Classes view providing information from endpoint resource

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,15 +3,26 @@
 
     angular
         .module('particip8WebApp')
-        .controller('HomeController', ['$scope', '$location', 'Classes', function ($scope, $location, Classes) {
+        .controller('HomeCtrl', ['$scope', '$location', 'Classes', function ($scope, $location, Classes) {
             $scope.classes = [];
 
+            //get all classes from Classes resource and format response using Array.map method
             Classes.query(function(data){
 
-                console.log(data);
+                $scope.classes = data.map(function(obj){
+                    return {
+                        id: obj.id,
+                        name: obj.name,
+                        days: "MWF (UPDATE THIS)",
+                        start_time: obj.start_time,
+                        end_time: obj.end_time 
+                    }
+                })
 
+                //ZACK: uncomment these to see the differences in the response data and what was formatted 
+                //console.log(data)
+                //console.log($scope.classes);
             });
-
 
             $scope.manageClass = function(classroom)
             {
