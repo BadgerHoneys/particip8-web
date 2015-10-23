d394fc5f9cd8a17605c32b951b89075701301f11
Josh Woodward <jdw6359@rit.edu>
2015-10-23 00:48:03 -0400

    Authentication

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,40 @@
 
     angular
         .module('particip8WebApp')
-        .controller('MainCtrl', ['$scope', '$cookies', '$location', function ($scope, $cookies, $location) {
+        .controller('MainCtrl', ['$scope', '$cookies', '$location', 'Users', function ($scope, $cookies, $location, Users) {
 
         	$scope.user = {
-        		username: "",
+        		email: "",
         		password: ""
         	}
 
-            $cookies.put("teacher_id", "2");
+            var user_id = $cookies.get("user_id")
 
-            $scope.login = function () {
-            	$location.path('/classes');
+            //TODO: replace this check with a POST providing the session_token
+            if(user_id){
+
+                $location.path("/classes");
+
+                //TODO: use this new url when app.js implemented
+                //$location.path("/classes/" + user_id);
+                //Also, refactor into helper method
+            }
+
+            $scope.login = function () {        
+                Users.authenticate($scope.user, function(user){
+                    if(user.id){
+                        //write the cookie
+                        $cookies.put("user_id", user.id)
+
+                        //TODO: use this new url when app.js implemented
+                        //$location.path("/classes/" + user_id);
+                        //Also, refactor into helper method
+                        $location.path("/classes");
+                    }
+                },function(err){
+                    alert("There was an error with authentication\nPlease provide a valid email and password");
+                });
+
             };
 
         }]);
