9ea2f7ad55f4f7118cb7f60658b5072659e9c965
Josh Woodward <jdw6359@rit.edu>
2015-10-25 19:57:58 -0400

    Evaluation Template creation

--- app/scripts/controllers/evaluation_template.js
+++ app/scripts/controllers/evaluation_template.js
@@ -4,16 +4,15 @@
     angular
         .module('particip8WebApp')
         .controller('EvaluationTemplateCtrl', ['$scope', '$location', '$cookies', '$http', '$routeParams', 
-            'RatingTypes', 'EvaluationTypes',
-            function ($scope, $location, $cookies, $http, $routeParams, RatingTypes, EvaluationTypes) {
+            'RatingTypes', 'EvaluationTypes', 'EvaluationTemplates',
+            function ($scope, $location, $cookies, $http, $routeParams, RatingTypes,
+            EvaluationTypes, EvaluationTemplates) {
             
             //TODO: Find out how to globalize this
             //replicate this across all controllers
             $http.defaults.headers.common["Auth-Token"] = $cookies.get("auth_token")
 
-            $scope.class_id = $routeParams.class_id;
-            $scope.evaluation_template_id = $routeParams.evaluation_template_id;            
-
+            // Determine if we are going to be editing the evaluation template
             if($scope.evaluation_template_id){
                 console.log("editing!!!");
 
@@ -22,10 +21,11 @@
                 $scope.edit = true;
             }
 
-            $scope.eval = {
+            $scope.evaluation_template = {
                 name: "",
                 evaluation_type_id: "",
-                scale_id: ""
+                rating_type_id: "",
+                school_class_id: ""
             }
 
             $scope.evaluationTypes = [];
@@ -46,15 +46,16 @@
                 console.log(err);
             });
 
-            //if we are in edit mode, then make a request to the the evaluation template
+            $scope.createEvaluation = function(evaluation_template) {            
+                //TODO: refactor this -- why arent we able to set $scope.school_class_id at page load time?                
+                evaluation_template.school_class_id = $routeParams.class_id
 
-            $scope.createEvaluation = function() {
-                // build out the array from the evaluation template's form
-                // send POST or PUT depending on if the evaluation was new or edited
-
-                // $location.path('/class/' + $scope.class_id);
-            
-                console.log("creating the evaluation");
+                EvaluationTemplates.save(evaluation_template, function(res){
+                    console.log("success");
+                    $location.path("/class/" + $routeParams.class_id);
+                }, function(err){
+                    console.log("error: " + err)
+                });
             }
 
             $scope.editEvaluation = function(){
---
+++ app/services/evaluation_templates.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('particip8WebApp')
+        .factory('EvaluationTemplates', EvaluationTemplatesFactory);
+
+    function EvaluationTemplatesFactory($resource) {
+        return $resource('http://localhost:3000/evaluation_templates/:id', {}, {});
+    };
+})();
