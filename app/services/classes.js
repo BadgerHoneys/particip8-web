2997548a5c1cfe29dabccb9952ad23f4227c0362
Zack Downs <zjd2035@rit.edu>
2015-11-08 13:25:02 -0500

    Added functionality to preselect an evaluation template's rating type, among other things.

--- app/services/classes.js
+++ app/services/classes.js
@@ -6,7 +6,21 @@
         .factory('Classes', ClassesFactory);
 
     function ClassesFactory($resource) {
-        return $resource('http://localhost:3000/school_classes/:id', {}, 
-        { query: {method:'GET',isArray:true}});
+        return $resource('http://localhost:3000/school_classes/:id', {id: '@id'}, 
+        {
+        	available: {
+        		url: 'http://localhost:3000/school_classes/available',
+        		method:'GET',
+        		isArray: true
+            },
+            remove_teacher: {
+                url: 'http://localhost:3000/school_classes/:id/remove_teacher',
+                method:'PATCH'
+            },
+            set_teacher: {
+                url: 'http://localhost:3000/school_classes/:id/set_teacher',
+                method:'PATCH'
+            }
+    	});
     };
 })();
